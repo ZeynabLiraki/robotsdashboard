b8f4b4ef8ef42e0819df679f9cc238b427bcc9e1
ZeynabLiraki <dev.zeynab.liraki@gmail.com>
2025-07-08 09:55:04 +0200

    improved the performance by adding javascript compression and inproved SEO for the name of project by adding meta

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import compress from "vite-plugin-compression";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    compress({
+      verbose: true,
+      disable: false,
+      threshold: 10240,
+      algorithm: "gzip",
+      ext: ".gz",
+    }),
+  ],
+});
