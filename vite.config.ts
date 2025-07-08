8f175767f91c890eca37bf6e67f28adf9106c6df
ZeynabLiraki <dev.zeynab.liraki@gmail.com>
2025-07-08 09:57:04 +0200

    removed some details

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import compress from "vite-plugin-compression";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
